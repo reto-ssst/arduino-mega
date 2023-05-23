d3838b8766ef697cefd0899e708da535474f69a2
Ambrosini Reto <128051061+reto-ssst@users.noreply.github.com>
2023-05-23 13:38:42 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <Arduino.h>
 
+#define VCC 4.972	// tensione di alimentazione
+
+// potenziometri
+#define POT_1 A6
+#define POT_2 A7
+
+// tasti
+#define S_1 62
+#define S_2 63
+
+//LED
+#define LED_PWM 12
+
 void setup() 
 {
   // put your setup code here, to run once:
